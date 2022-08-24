b175f3e82d2401c57b6c65f3f63760d4cfa16a69
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-08-24 09:32:01 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,7 +261,7 @@ copyBtn = document.querySelector(".copy");
 
 function fetchRequest(file, formData) {
     infoText.innerText = "Scanning QR Code...";
-    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    fetch("https://api.qrserver.com/v1/read-qr-code/", {
         method: 'POST', body: formData
     }).then(res => res.json()).then(result => {
         result = result[0].symbol[0].data;
